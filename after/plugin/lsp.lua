7274caf71fee20b95e51d4c63f6328f598cef57e
Thanos Dimitriades <thanosngplus@gmail.com>
2023-10-16 19:00:06 +0300

    add tab shift tab for intellisense

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,8 +38,8 @@ cmp.setup({
   },
   formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
