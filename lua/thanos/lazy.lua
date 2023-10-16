1460fc3f9c6a4ae47383a085038427b4ce418a3f
Thanos Dimitriades <thanosngplus@gmail.com>
2023-10-16 18:51:21 +0300

    update lsp

--- lua/thanos/lazy.lua
+++ lua/thanos/lazy.lua
@@ -4,33 +4,31 @@ vim.opt.rtp:prepend(lazypath)
 local ok, lazy = pcall(require, 'lazy')
 
 if not ok then
-  local msg = 'You need to install the plugin manager lazy.nvim\n'
+    local msg = 'You need to install the plugin manager lazy.nvim\n'
     .. 'in this folder: ' .. lazypath
 
-  print(msg)
-  return
+    print(msg)
+    return
 end
 
 lazy.setup({
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    --- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
-  -- LSP Support
-  {'neovim/nvim-lspconfig'},
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-
-  -- Autocompletion
-  {'hrsh7th/nvim-cmp'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'L3MON4D3/LuaSnip'},
-  "rebelot/kanagawa.nvim",
-  {
-	"nvim-telescope/telescope.nvim", tag = "0.1.3",
-	dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  "ThePrimeagen/harpoon",
-  {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate"
-  },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    "rebelot/kanagawa.nvim",
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.3",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    "ThePrimeagen/harpoon",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
 })
